cc0abf7df44357ec934ff584734a90449adeeb58
sadohu <sadohu26@yahoo.com>
2023-11-24 13:41:32 -0500

    [Refactor] Improve leap year logic

--- src/main/java/pe/edu/cibertec/Tema2Sesion1b.java
+++ src/main/java/pe/edu/cibertec/Tema2Sesion1b.java
@@ -3,19 +3,26 @@ package pe.edu.cibertec;
 public class Tema2Sesion1b {
 
     public static boolean isLeap(int year) {
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return true;
-            }
-        } else {
+
+        if ( (year % 4 == 0) && !(year % 100 == 0) || (year % 400 == 0) ) {
+           return true;
+        }else{
             return false;
         }
+
+//        if (year % 4 == 0) {
+//            if (year % 100 == 0) {
+//                if (year % 400 == 0) {
+//                    return true;
+//                } else {
+//                    return false;
+//                }
+//            } else {
+//                return true;
+//            }
+//        } else {
+//            return false;
+//        }
     }
 
 }
