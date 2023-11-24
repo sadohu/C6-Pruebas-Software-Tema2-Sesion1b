16c7a4cecc415e09f974a21f39db70fefab43a35
sadohu <sadohu26@yahoo.com>
2023-11-24 13:39:13 -0500

    [Add] Class for Leap Test

---
+++ src/main/java/pe/edu/cibertec/Tema2Sesion1b.java
@@ -0,0 +1,21 @@
+package pe.edu.cibertec;
+
+public class Tema2Sesion1b {
+
+    public static boolean isLeap(int year) {
+        if (year % 4 == 0) {
+            if (year % 100 == 0) {
+                if (year % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
+    }
+
+}
