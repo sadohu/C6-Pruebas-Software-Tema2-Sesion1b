833eed0190fd89c49c57f192720a9c1f4b20ddf9
sadohu <sadohu26@yahoo.com>
2023-11-24 16:34:56 -0500

    [Fix] Missing Test Annotation

--- src/test/java/pe/edu/cibertec/Tema2Sesion1bTest.java
+++ src/test/java/pe/edu/cibertec/Tema2Sesion1bTest.java
@@ -11,23 +11,33 @@ class Tema2Sesion1bTest {
         assertTrue(Tema2Sesion1b.isLeap(2000));
     }
 
+    @Test
     public void passwordLessThan6CaractersAssertFalse(){
         assertFalse(Tema2Sesion1b.pciPass("aaaab"));
     }
 
+    @Test
     public void passwordMoreThan10CaractersAssertFalse(){
         assertFalse(Tema2Sesion1b.pciPass("1234567890"));
     }
 
+    @Test
     public void passwordContainsJustLettersAssertFalse(){
         assertFalse(Tema2Sesion1b.pciPass("aaaabbbbc"));
     }
 
+    @Test
     public void  passwordContainsJustLetterAndNumbersAssertFalse(){
         assertFalse(Tema2Sesion1b.pciPass("aaaa1234bb"));
     }
 
+    @Test
+    public void passwordContainsSpecialCaractersAssertFalse(){
+        assertFalse(Tema2Sesion1b.pciPass("aaaa_bbb12"));
+    }
+
+    @Test
     public void passwordContainsLettersNumbersAndCapitalLetterAssertTrue(){
-        assertTrue(Tema2Sesion1b.pciPass("Aaaabbbb12"));
+        assertTrue(Tema2Sesion1b.pciPass("aaaaBbbb12"));
     }
 }
